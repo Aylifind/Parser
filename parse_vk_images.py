3751d6970e65ff6202b973ac96a13c4f76d0cdd5
Andrey <timebird@yandex.ru>
2017-11-25 10:24:45 +0300

    Update parse_vk_images.py

--- parse_vk_images.py
+++ parse_vk_images.py
@@ -1,5 +1,5 @@
 """
-Данный скрипт принимает на вход логин и пароль ВК, а на выходе формирует датафрейм
+Данный скрипт принимает на вход логин и пароль ВК, а на выходе формирует .json
 с фотографиями cо стены указанного пользователем сообщества ВК.
 """
 
@@ -11,9 +11,16 @@ from auth import *
 import json
 import pandas as pd
 import numpy as np
+import wget
 
 
 # Понаставить исключений на все случаи ошибок
+# Например, чтобы не вводили кириллицей ID
+# И вообще надо написать функцию, которая на вход принимает ссылку на группу в enter_the_vk_groups отдаёт только id
+# если нет папки images, то ее надо создать
+# Желательно формата images_<#_группы>
+
+
 
 def create_vk_groups_list():
     """Здесь тупо создаём пустой список"""
@@ -26,37 +33,36 @@ def enter_the_vk_groups(all_vk_groups):
     Здесь вбиваем id-ники от групп. Чтобы не было переинициализации списка (массива) при рекурсии,
     передаём его в качестве параметра от предыдущей функции
     """
-    vk_group = input('\nEnter the group ID:\n')
+
+    """На вход принимается ссылка на группу, а отдаётся только id"""
+    vk_group = input('\nEnter the group ID:\n').split('https://vk.com/')[1] # здесь отсекается часть zaokskypr
+    try:
+        vk_group = vk_group.split('public')[1] # здесь отсекается часть public у public153967240 при наличии слова public
+        vk_group = int(vk_group)
+    except:
+        pass
+
+
     all_vk_groups = np.append(all_vk_groups, vk_group)
     answer = input('Is it all? Print 1 if that is all or 0 otherwise:\n')
     if answer == '1':
-	return all_vk_groups
+        return all_vk_groups
     else:
-	return enter_the_vk_groups(all_vk_groups)
+        return enter_the_vk_groups(all_vk_groups)
 
-
-def isint(s):
-    """Проверка на число"""
-    try:
-	int(s)
-        return True
-    except ValueError:
-	return False
 
 
 def request_photos(vk_group, login, password):
     session = vk.AuthSession(
-    app_id=6169227, 
-    user_login=login,
-    user_password=password, 
-    scope='photos'
-    )
+        app_id=6169227, 
+        user_login=login,
+        user_password=password, 
+        scope='photos'
+        )
     api = vk.API(session)
-    if isint(vk_group) is False:  # на случай, если страница вида vk.com/abcdef
-	vk_group = api.groups.getById(group_ids=vk_group, fiels='id')
-	vk_group = str(vk_group[0].get('gid'))
+    vk_group = api.groups.getById(group_ids=vk_group, fiels='id')
+    vk_group = str(vk_group[0].get('gid'))
     photo_request = api.photos.get(owner_id='-'+vk_group, album_id='wall') # '-153967240, -67878782, zaokskypr' для сообществ. Для аккаунтов без "-".
-    #print(photo_request) # посмотреть сформировавшийся файл
     return photo_request
 
 
@@ -64,13 +70,18 @@ def transform_to_df(photo_request):
     srcs = np.empty(0)
     owners = np.empty(0)
     for item in photo_request:
-	srcs = np.append(srcs, item['src']) # src == photo
-	owners = np.append(owners, int(abs(item['owner_id']))) # чтобы не ставилась "-" перед ID группы
+        srcs = np.append(srcs, item['src_big']) # src - это прямая ссылка на фото. ЕСТЬ: src, src_big, src_small, src_xbig, src_xxxbig
+        owners = np.append(owners, int(abs(item['owner_id']))) # чтобы не ставилась "-" перед ID группы
     df = pd.DataFrame({
-	'Groups': owners,
-	'Photos': srcs
-	})
+        'Groups': owners,
+        'Photos': srcs
+        })
     return df
+
+
+def download_images(df):
+    for link in df['Photos']:
+        wget.download(link, 'images/')
 
 
 def main():
@@ -78,16 +89,11 @@ def main():
     password = get_user_password()
     groups_list = create_vk_groups_list()
     vk_groups_input = enter_the_vk_groups(groups_list)
-    for vk_group in vk_groups_input:	
+    for vk_group in vk_groups_input:
         photo_request = request_photos(vk_group, login, password)
-	df = transform_to_df(photo_request)
-
-
-	
+        df = transform_to_df(photo_request)
+        download_images(df)
 
 
 if __name__ == '__main__':
     main()
-
-
-
