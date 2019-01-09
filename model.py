154d40632ce90c11a6e09c30dfac354622110af5
Andrey <timebird@yandex.ru>
2019-01-10 02:03:13 +0300

    Update model.py

--- model.py
+++ model.py
@@ -39,7 +39,10 @@ for model in MODELS.keys():
 
     for image_path in image_paths:
         print("[INFO]: Loading and preprocessing image...")
-        image = load_img(image_path, target_size=inputShape)
+        try:
+            image = load_img(image_path, target_size=inputShape)
+        except:
+            continue
         image = img_to_array(image)
         image = np.expand_dims(image, axis=0)
         image = preprocess(image)
@@ -49,40 +52,5 @@ for model in MODELS.keys():
         preds = model.predict(image)
         P = imagenet_utils.decode_predictions(preds)
 
-        orig = cv2.imread(image_path)
-        text_place_col = 10 # начальное положение надписи
-        text_place_str = 30
-        сounter = 0 # счётчик для добавления подписей на рисунок
-        image_num = 0 # счётчик самих изображений
         for (i, (imagenetID, label, prob)) in enumerate(P[0]):
-            print(i, (imagenetID, label, prob))
-            сounter += 1
-            #print('{}. {}: {:.2f}%'.format(i+1, label, prob*100))
-            cv2.putText(orig, 'Label: {}, {:.2f}%'.format(label, prob*100), (text_place_col, text_place_str), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
-            text_place_col += 0 # сдвиг следующей надписи по столбцам
-            text_place_str += 20 # сдвиг следующей надписи по строкам
-            if сounter == len(MODELS.keys()): # если мы пробежали и проставили вероятности для всех моделей - сохраняем картинку
-                cv2.imwrite(folderpath + f'{image_num}_.png', orig)
-                image_num += 1
-
-
-
-        
-        #(imagenetID, label, prob) = P[0][0]
-        #print(label, prob)
-        #
-        #cv2.imshow('Classification: ', orig)
-        #cv2.waitKey(0)
-
-
-
-
-
-
-
-
-
-
-
-
-
+            print('{}. {}: {:.2f}%'.format(i+1, label, prob*100))
