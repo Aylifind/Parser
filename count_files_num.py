942a1b1906c86cab2a510ae3d9fb58ce95040c21
Andrey <timebird@yandex.ru>
2019-03-19 23:50:07 +0300

    Update count_files_num.py

--- count_files_num.py
+++ count_files_num.py
@@ -1,13 +1,24 @@
 # Скрипт для подсчета количества изображений в папках
 import os
+import numpy as np
 
 
-image_paths = []
+folderpath = '/Users/user/Desktop/21-1000/'
 
-folderpath = '/Volumes/WD_NEW/cats/'
 
+init_folders_num = []
 c = 0
 for dirpath, dirnames, filenames in os.walk(folderpath):
-    for filename in filenames:
-        c += 1
-print(c)
+    path, dirs, files = next(os.walk(dirpath))
+    file_count = len(files)
+    init_folders_num.append(file_count)
+
+#print('Init folders num: ', len(init_folders_num))
+#print('Init number of images: ', sum(init_folders_num))
+#print('Median number of images: ', np.mean(init_folders_num))
+
+folders_num_after_filtering = [elem for elem in init_folders_num if elem > 1 and elem < 100]
+print('Folders num after filtering: ', len(folders_num_after_filtering))
+print('Number of images after filtering: ', sum(folders_num_after_filtering))
+print('Mean of images after filtering: ', np.mean(folders_num_after_filtering))
+
