579c46eba81c0f34c44baff78ffec1e345a21bf9
Andrey <timebird@yandex.ru>
2019-08-10 15:47:57 +0300

    Update upload.py

--- website/upload.py
+++ website/upload.py
@@ -1,12 +1,17 @@
 from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
-from werkzeug.utils import secure_filename
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 
-UPLOAD_FOLDER = './static/uploads'    # папка, куда будут загружаться файлы с сайта
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/user/Desktop/website/database_files/filesstorage.db'
+db = SQLAlchemy(app)
+
+
+class FileContents(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(300))   # 300 - максимальная длина строки
+    data = db.Column(db.LargeBinary)
 
 
 @app.route('/')
@@ -16,6 +21,7 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    db.create_all()   # создать базу данных
     if request.method == 'POST':
         lost_or_found = request.form.get('lost_found')
         pet = request.form.get('pet')
@@ -23,25 +29,25 @@ def upload_file():
         date = request.form.get('date')
         color = request.form.get('color')
         addit_info = request.form.get('addit_info')
-    
         files = request.files.getlist('files[]')
         for file in files:
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            newFile = FileContents(name=file.filename, data=file.read())
+            db.session.add(newFile)
+            db.session.commit()
 
         return '''
                 Upload completed. <br> 
                 <br>
+                <b>File saved: {}</b> <br>
                 Lost or found: {} <br>
                 Pet: {} <br>
                 Location: {} <br>
                 Date: {} <br>
                 Color: {} <br>
                 Addit info: {} <br>
-               '''.format(lost_or_found, pet, location, date, color, addit_info)
+               '''.format(file.filename, lost_or_found, pet, location, date, color, addit_info)
     return render_template('upload.html')
-
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
