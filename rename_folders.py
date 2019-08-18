e34e0af51a0c38c9138219f4e87002758d41078c
Andrey <timebird@yandex.ru>
2019-08-18 19:26:01 +0300

    Add files via upload

---
+++ rename_folders.py
@@ -0,0 +1,18 @@
+import os
+
+path_to_folder = '/Users/user/Desktop/<your_folder>/'
+
+
+def rename(path_to_folder):
+
+    i = 1   # first folder name
+    for folder in os.listdir(path_to_folder):
+        # If folder is not a folder
+        if not os.path.isdir(os.path.join(path_to_folder, folder)):
+            continue
+        os.rename(os.path.join(path_to_folder, folder), os.path.join(path_to_folder, '{}'.format(i)))
+        i += 1
+
+
+if __name__ == '__main__':
+    rename(path_to_folder)
