86f08d1418a4413cba1ad7b13494dd367a5ddf72
Andrey <timebird@yandex.ru>
2019-08-18 18:49:16 +0300

    Add files via upload

---
+++ rename_files.py
@@ -0,0 +1,15 @@
+import glob
+import os
+
+folder_path = '/Users/user/Desktop/Misha/'
+
+
+def rename(folder_path):
+	
+	for i, filename in enumerate(glob.glob(folder_path + '*.jpeg')):
+		os.rename(filename, os.path.join(folder_path, str(i) + '.jpeg'))
+
+
+
+if __name__ == '__main__':
+	rename(folder_path)
