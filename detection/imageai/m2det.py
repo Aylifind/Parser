524a97f5002aad572a09f7992271152c6a4a636b
Andrey <timebird@yandex.ru>
2019-08-29 00:53:38 +0300

    Create m2det.py

---
+++ detection/imageai/m2det.py
@@ -0,0 +1,102 @@
+#ORIGINAL: https://github.com/LeeDongYeun/keras-m2det
+# import keras
+import keras
+
+# import keras_retinanet
+from keras_m2det import models
+from keras_m2det.utils.image import read_image_bgr, preprocess_image, resize_image
+from keras_m2det.utils.visualization import draw_box, draw_caption
+from keras_m2det.utils.colors import label_color
+
+# import miscellaneous modules
+import matplotlib.pyplot as plt
+import cv2
+import os
+import numpy as np
+import time
+
+# set tf backend to allow memory to grow, instead of claiming everything
+import tensorflow as tf
+
+
+def get_session():
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    return tf.Session(config=config)
+
+# use this environment flag to change which GPU to use
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+# set the modified tf session as backend in keras
+keras.backend.tensorflow_backend.set_session(get_session())
+
+
+
+# adjust this to point to your downloaded/trained model
+# models can be downloaded here: https://github.com/fizyr/keras-retinanet/releases
+model_path = os.path.join('..', 'snapshots', 'resnet50_coco_best_v2.1.0.h5')
+
+# load retinanet model
+model = models.load_model(model_path, backbone_name='resnet50')
+
+# if the model is not converted to an inference model, use the line below
+# see: https://github.com/fizyr/keras-retinanet#converting-a-training-model-to-inference-model
+#model = models.convert_model(model)
+
+#print(model.summary())
+
+# load label to names mapping for visualization purposes
+labels_to_names = {15: 'cat', 33:'kitte'} #16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
+
+
+from skimage.io import imshow, imread, imsave
+
+
+path_to_images_folder = '/Users/user/Desktop/aylifind/detection/cats_original/'
+
+
+def make_detection(i, image_path):
+    
+    image = imread(image_path)
+    # copy to draw on
+    draw = image.copy()
+    draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+
+    # preprocess image for network
+    image = preprocess_image(image)
+    image, scale = resize_image(image)
+
+    # process image
+    start = time.time()
+    boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
+    print("processing time: ", time.time() - start)
+
+    # correct for image scale
+    boxes /= scale
+
+    # visualize detections
+    for box, score, label in zip(boxes[0], scores[0], labels[0]):
+        # scores are sorted so we can break
+        if score < 0.5:
+            break
+
+        color = label_color(label)
+        try:
+            b = box.astype(int)
+            draw_box(draw, b, color=color)
+
+            caption = "{} {:.3f}".format(labels_to_names[label], score)
+            draw_caption(draw, b, caption)
+            x1 = b[0]
+            y1 = b[1]
+            x2 = b[2]
+            y2 = b[3]
+            img = image[y1:y2, x1:x2]
+        except:
+            continue
+    imsave('/Users/user/Desktop/aylifind/detection/m2det_e/' + 'image_{}.png'.format(i), img)
+    
+
+for i, image in enumerate(os.listdir(path_to_images_folder)):
+    if image.endswith('.jpg'):
+        make_detection(i, path_to_images_folder + image)
